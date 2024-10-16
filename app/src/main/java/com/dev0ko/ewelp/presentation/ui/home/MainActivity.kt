c8d3fd96d75b25f95a9ed3e1095af7f8cbaf69e1
mobelite-fkchaou <144110474+mobelite-fkchaou@users.noreply.github.com>
2024-10-16 12:05:29 +0100

    feat(Auth Library): - Display the profile image #feat /auth_library

--- app/src/main/java/com/dev0ko/ewelp/presentation/ui/home/MainActivity.kt
+++ app/src/main/java/com/dev0ko/ewelp/presentation/ui/home/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dev0ko.ewelp.presentation.ui.home
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import androidx.activity.viewModels
@@ -8,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.dev0ko.authlib.presentation.viewmodel.AuthenticationViewModel
 import com.dev0ko.ewelp.data.entity.Photo
 import com.dev0ko.ewelp.databinding.ActivityMainBinding
 import com.dev0ko.ewelp.presentation.ui.adapter.CategoriesAdapter
@@ -16,12 +19,17 @@ import com.dev0ko.ewelp.presentation.ui.adapter.RecommendedAdapter
 import com.dev0ko.ewelp.presentation.ui.image.ListOfImagesActivity
 import com.dev0ko.ewelp.presentation.viewmodel.PhotoViewModel
 import com.dev0ko.ewelp.utils.Constants
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
+import java.net.URL
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val photoVM: PhotoViewModel by viewModels()
+    private val authenticationViewModel: AuthenticationViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +51,15 @@ class MainActivity : AppCompatActivity() {
                 false
             }
         }
+
+        binding.profileImage.setOnClickListener {
+            authenticationViewModel.logout()
+        }
+        authenticationViewModel.getConnectedUser().observe(this, Observer {
+            if (it?.profilePictureUrl != null) {
+                Picasso.get().load(it?.profilePictureUrl).into(binding.profileImage)
+
+            }})
 
 
         photoVM.getRandomImages().observe(this, Observer {
@@ -79,4 +96,5 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
 }
